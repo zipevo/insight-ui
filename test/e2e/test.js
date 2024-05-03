f27da3caf8de8cf5e1e516a120a67a9e3d3ed666
pshenmic <pshenmic@gmail.com>
2024-05-03 17:31:04 +0700

    Migrate to mocha + selenium webdriver

--- test/e2e/test.js
+++ test/e2e/test.js
@@ -1,64 +1,25 @@
 const { expect } = require('chai');
-const startInsightUI = require('../../lib/test/startInsightUI');
+const {
+  Builder, Browser,
+} = require('selenium-webdriver');
 const wait = require('../../lib/test/util/wait');
 
-const topPanel = require('../../lib/test/pages/TopPanel');
-const blockPage = require('../../lib/test/pages/BlockPage');
-const statusPage = require('../../lib/test/pages/StatusPage');
-
-const InsightUIOptions = require('../../lib/test/service-ctl/InsightUIOptions');
-
-let originalTimeout;
+const TopPanel = require('../../lib/test/pages/TopPanel');
+const StatusPage = require('../../lib/test/pages/StatusPage');
+const BlockPage = require('../../lib/test/pages/BlockPage');
 
 describe('basic UI tests', () => {
-  let masterNode;
-
   let url;
+  let browser;
   let blockHash;
+  let topPanel;
+  let statusPage;
+  let blockPage;
   let trxHash;
 
-  beforeAll(async () => {
-    const rootPath = process.cwd();
-
-    const insightUIContainerOptions = {
-      volumes: [
-        `${rootPath}/dashcore-node:/insight/node_modules/@dashevo/insight-ui/dashcore-node`,
-        `${rootPath}/po:/insight/node_modules/@dashevo/insight-ui/po`,
-        `${rootPath}/public:/insight/node_modules/@dashevo/insight-ui/public`,
-      ],
-    };
-
-    const insighUIOptions = {
-      container: insightUIContainerOptions,
-    };
-
-    InsightUIOptions.setDefaultCustomOptions(insighUIOptions);
-
-
-    [masterNode] = await startInsightUI.many(1);
-
-    url = `http://127.0.0.1:${masterNode.insightUi.options.getUiPort()}/insight/`;
-
-    await masterNode.dashCore.getApi().generate(15);
-  });
-
-  afterAll(async () => {
-    const instances = [
-      masterNode,
-    ];
-
-    await Promise.all(instances.filter(i => i)
-      .map(i => i.remove()));
-  });
-
-  beforeEach(async () => {
-    await browser.waitForAngularEnabled(false);
-    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 250000;
-  });
-
-  afterEach(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  before(async () => {
+    browser = await new Builder().forBrowser(Browser.CHROME).build();
+    url = 'http://insight.testnet.networks.dash.org:3001/insight/';
   });
 
   describe('Home Page', () => {
@@ -70,16 +31,20 @@ describe('basic UI tests', () => {
     });
 
     it('should be able to open block page', async () => {
+      topPanel = new TopPanel(browser);
       await topPanel.openBlockPage();
       const title = await browser.getTitle();
       expect(title).equal('Home | Insight');
     });
+
 
     it('should be able to open status page', async () => {
       await topPanel.openStatusPage();
       await wait(10000); // time to complete sync
       const title = await browser.getTitle();
       expect(title).equal('Status | Insight');
+
+      statusPage = new StatusPage(browser);
 
       const syncProgress = await statusPage.getSyncProgress();
       expect(syncProgress).equal('100% Complete');
@@ -114,14 +79,14 @@ describe('basic UI tests', () => {
       const protocolVersion = await statusPage.getProtocolVersion();
       expect(Number.isInteger(parseInt(protocolVersion, 10))).equal(true);
 
-      const blocks = await statusPage.getBlocks();
-      expect(blocks).equal('15');
+      // const blocks = await statusPage.getBlocks();
+      // expect(blocks).equal('15');
 
       const timeOffset = await statusPage.getTimeOffset();
       expect(timeOffset).equal('0');
 
-      const connections = await statusPage.getConnections();
-      expect(connections).equal('0');
+      // const connections = await statusPage.getConnections();
+      // expect(connections).equal('0');
 
       const miningDifficulty = await statusPage.getMiningDifficulty();
       expect(miningDifficulty).not.equal('');
@@ -135,11 +100,14 @@ describe('basic UI tests', () => {
       const infoErrors = await statusPage.getInfoErrors();
       expect(infoErrors).equal('');
     });
+
     it('should be able to route to block number', async () => {
       const blockIdToSearch = '12';
 
       await browser.get(`${url}block/${blockIdToSearch}`);
       await wait(10000);
+
+      blockPage = new BlockPage(browser);
 
       const currentUrl = await browser.getCurrentUrl();
       expect(currentUrl).equal(`${url}block/${blockIdToSearch}`);
@@ -152,10 +120,15 @@ describe('basic UI tests', () => {
 
       expect(nextBlock).equal(`${parseInt(blockId, 10) + 1}`);
     });
+
     it('should be able search by block number', async () => {
       const blockIdToSearch = '12';
+      topPanel = new TopPanel(browser);
 
       topPanel.search(blockIdToSearch);
+      await wait(5000);
+      blockPage = new BlockPage(browser);
+
       const currentUrl = await browser.getCurrentUrl();
       const blockId = (await blockPage.getBlockId()).replace('Block #', '');
       expect(blockId).equal(blockIdToSearch);
